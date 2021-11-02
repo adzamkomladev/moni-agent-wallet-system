f44c116c5a2dec3d3238081a16cba7d6be8c2496
Komla Adzam <adzamkomla.dev@gmail.com>
2021-11-02 15:10:20 +0000

    Add withdraw from wallet

--- src/routes/wallet.js
+++ src/routes/wallet.js
@@ -1,11 +1,12 @@
 import express from "express";
 
-import { topUpWallet } from "../services/wallet";
+import { topUpWallet, withdrawFromWallet } from "../services/wallet";
 
 function getWalletRoutes() {
   const router = express.Router();
 
   router.post("/:id/top-up", topUp);
+  router.post("/:id/withdraw", withdraw);
 
   return router;
 }
@@ -36,9 +37,55 @@ async function topUp(req, res) {
       },
     });
   } catch (error) {
-    const response = res;
+    let response = res;
 
     if (error.message === "Wallet not found") {
+      response = res.status(400);
+    } else if (error.message === "Failed to top up") {
+      response = res.status(500);
+    } else {
+      response = res.status(500);
+    }
+
+    return response.json({
+      message: error.message,
+    });
+  }
+}
+
+async function withdraw(req, res) {
+  const body = req.body;
+  const id = req.params.id;
+
+  try {
+    const walletWithdrawalData = {
+      id,
+      amount: body.amount,
+      agentId: body.agentId,
+    };
+
+    const walletTransaction = await withdrawFromWallet(walletWithdrawalData);
+
+    return res.json({
+      amount: walletTransaction.amount,
+      balanceBefore: parseFloat(walletTransaction.balanceBefore),
+      balanceAfter: walletTransaction.balanceAfter,
+      type: walletTransaction.type,
+      description: walletTransaction.description,
+      status: walletTransaction.status,
+      createdAt: walletTransaction.createdAt,
+      wallet: {
+        currency: walletTransaction.wallet.currency,
+        walletNumber: walletTransaction.wallet.walletNumber,
+      },
+    });
+  } catch (error) {
+    let response = res;
+
+    if (
+      error.message === "Wallet not found" ||
+      error.message === "Insufficient balance"
+    ) {
       response = res.status(400);
     } else if (error.message === "Failed to top up") {
       response = res.status(500);
--- src/services/wallet.js
+++ src/services/wallet.js
@@ -52,5 +52,61 @@ async function topUpWallet(walletTopUpData) {
     connection?.close();
   }
 }
+async function withdrawFromWallet(walletWithdrawalData) {
+  let connection;
 
-export { topUpWallet };
+  try {
+    connection = await createConnection();
+
+    let { id, amount, agentId } = walletWithdrawalData;
+
+    const walletRepository = connection.getRepository("Wallet");
+    const wallet = await walletRepository.findOne({ where: { id } });
+
+    if (!wallet) {
+      throw new Error("Wallet not found");
+    }
+
+    amount = +amount;
+
+    if (amount > parseFloat(wallet.balance)) {
+      throw new Error("Insufficient balance");
+    }
+
+    const walletTransactionData = {
+      wallet,
+      amount,
+      balanceBefore: wallet.balance,
+      balanceAfter: parseFloat(wallet.balance) - amount,
+      type: "Debit",
+      description: "Withdraw from wallet",
+      status: "COMPLETED",
+    };
+
+    return await connection.transaction(async (transactionalEntityManager) => {
+      const results = await transactionalEntityManager.update(
+        "Wallet",
+        wallet.id,
+        {
+          balance: walletTransactionData.balanceAfter,
+        }
+      );
+
+      if (results.affected === 0) {
+        throw new Error("Failed to top up");
+      }
+
+      return await transactionalEntityManager.save(
+        "WalletTransaction",
+        walletTransactionData
+      );
+    });
+  } catch (error) {
+    console.log("Error topping up", error);
+    throw error;
+  } finally {
+    connection?.close();
+  }
+}
+
+export { topUpWallet, withdrawFromWallet };
